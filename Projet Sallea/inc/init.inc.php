08f7c650f17afb14668ff41e3cfd81b19e45f517
Luc Gerard <lucgerard10@gmail.com>
2017-05-22 12:18:49 +0200

    *

--- Projet Sallea/inc/init.inc.php
+++ Projet Sallea/inc/init.inc.php
@@ -1,2 +1,15 @@
 <?php 
 $pdo = new PDO('mysql:host=localhost;dbname=Sallea', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING, PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+
+// Session
+session_start();
+// chemin du site
+define('RACINE_SITE', '/Cours PHP/08site/'); // indique le dossier dans lequel se situe le site sans 'localhost'
+
+// Déclaration des variables d'affichage du site
+$contenu = '';
+$contenu_gauche = '';
+$contenu_droite = '';
+
+// Autres inclusions
+require_once('fonction.inc.php');
